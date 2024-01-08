cdb3d12b77c460af4db7ace54321e6577d6e9199
Maksym <makssemenhenko@gmail.com>
2024-01-08 16:10:13 +0100

    Initial commit

---
+++ src/main/java/de/maksym/crud/DAO/PersonDAO.java
@@ -0,0 +1,29 @@
+package de.maksym.crud.DAO;
+
+import de.maksym.crud.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Cat"));
+        people.add(new Person(++PEOPLE_COUNT, "Dog"));
+        people.add(new Person(++PEOPLE_COUNT, "Fox"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
