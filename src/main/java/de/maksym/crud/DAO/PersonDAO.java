dc98a87f74b009e53849c906372183d1c1c4c11d
Maksym <makssemenhenko@gmail.com>
2024-01-09 14:41:58 +0100

    New HTTP-forms delete and new.

--- src/main/java/de/maksym/crud/DAO/PersonDAO.java
+++ src/main/java/de/maksym/crud/DAO/PersonDAO.java
@@ -26,4 +26,13 @@ public class PersonDAO {
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void remove(String personName){
+        people.removeIf(person -> Objects.equals(person.getName(), personName));
+    }
 }
